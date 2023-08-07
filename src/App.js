dd350cf6821e1cf740fd09ed47ed6672508103f4
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-08-07 04:08:26 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
 import './App.css';
+import React from 'react';
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div >
-
+    <div className="App">
+    <Routes>
+      <Route
+        path="/" element={ <HomePage /> } />
+    </Routes>
     </div>
-
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import beersImg from "../assets/beers.png"
+import randomBeer from "../assets/random-beer.png"
+import newBeer from "../assets/new-beer.png"
+
+function HomePage() {
+    return (
+        <div className="home-page">
+            <div>
+                    <img className="home-img" src={beersImg} alt="beers" /> <br />
+                    <Link className="link-name" to="/beers">All Beers </Link>
+                    <p className="home-page-p">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe autem
+                    numquam molestiae odit, atque quaerat dolor quisquam eligendi voluptas
+                    alias quo deleniti, accusantium, quis commodi! Eveniet eligendi alias
+                    corporis id.
+                    </p>
+            </div>
+
+            <div>
+                    <img className="home-img" src={randomBeer} alt="beers" /> <br />
+                    <Link className="link-name" to="/random-beer">Random Beer</Link>
+                    <p className="home-page-p">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe autem
+                    numquam molestiae odit, atque quaerat dolor quisquam eligendi voluptas
+                    alias quo deleniti, accusantium, quis commodi! Eveniet eligendi alias
+                    corporis id.
+                    </p>
+            </div>
+
+            <div>
+                    <img className="home-img" src={newBeer} alt="beers" /> <br />
+                    <Link className="link-name" to="/new-beers">New Beer</Link>
+                    <p className="home-page-p">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe autem
+                    numquam molestiae odit, atque quaerat dolor quisquam eligendi voluptas
+                    alias quo deleniti, accusantium, quis commodi! Eveniet eligendi alias
+                    corporis id.
+                    </p>
+            </div>
+
+        </div>
+    );
+    }
+
+export default HomePage;
